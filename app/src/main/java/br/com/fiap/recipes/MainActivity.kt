f65e45980ca8b0464b988ec08e8a7c7637e4bfdf
Pedro H. M. de Souza <pmartinshenrique@gmail.com>
2026-08-06 22:48:18 -0300

    Primeiro commit da aplicaÃção, já incluindo o preview da InitialScren e a inserção de uma imagem

---
+++ app/src/main/java/br/com/fiap/recipes/MainActivity.kt
@@ -0,0 +1,52 @@
+package br.com.fiap.recipes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.recipes.ui.theme.RecipesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipesTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun InitialScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Cyan)
+    ){
+        Image(
+            painter = painterResource(R.drawable.chef),
+            contentDescription = "Imagem de um Chef de cozinha",
+            modifier = Modifier.align(alignment = Alignment.TopCenter)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun InitialScreenPreview(){
+    RecipesTheme() {
+        InitialScreen()
+    }
+}
